3ae300ccd52a1f92a9786eff346a4c99b531dc32
Wesley Ribeiro <wesleyjcr@gmail.com>
2020-10-28 17:58:21 -0300

    Um bot para monitorar as doaçoes da operaçao FIFA

--- operacaofifa/app.py
+++ operacaofifa/app.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from operacaofifa.ext import database
 from operacaofifa.ext import database_mongo
@@ -6,10 +8,10 @@ from operacaofifa.blueprints import bpbot
 
 def create_app():
     app = Flask(__name__)
-
+    load_dotenv('.env')
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../storage.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["MONGO_URI"] = 'mongodb+srv://telegram_bot:BOToperacaoFIFA2020@cluster0.3pee7.mongodb.net/operacaofifa?retryWrites=true&w=majority'
+    app.config["MONGO_URI"] = os.getenv('MONGO_URI')
     database.init_app(app)
     database_mongo.init_app(app)
     bpbot.init_app(app)
